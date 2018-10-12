535323488e6b71b4c6afc5ccc7db1158f466227e
David masterson <david73180@gmail.com>
2018-10-12 19:40:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,66 +23,66 @@ class Song
     return hello
   end
 
-  def self.new_by_name(name)
-    song = Song.new(name)
-  end
-
-  def self.create_by_name(name)
-    song = Song.new(name)
-  end
-
-  def self.find_by_name(name)
-    @@all.find {|x| x.name == name}
-  end
-
-  def self.find_or_create_by_name(name)
-    song = Song.new(name)
-    @@all.find {|x| x.name == name}
-  end
-
-  def self.alphabetical
-    @@all = @@all.sort do |a, b|
-      a.name <=> b.name
-      if a.name == b.name
-        0
-      elsif a.name < b.name
-        -1
-      elsif a.name > b.name
-        1
-      end
-    end
-    return @@all
-  end
-
-  def self.new_from_filename(mp3)
-    name = mp3.split(" - ")
-    artist_name = name[0]
-    name = name[1].gsub(".mp3", "")
-    song = Song.new(name)
-    song.artist_name = artist_name
-    return song
-  end
-
-
-  def self.destroy_all
-    all.clear
-  end
-
-  def self.create_from_filename(mp3)
-    name = mp3.split(" - ")
-    artist_name = name[0]
-    name = name[1].gsub(".mp3", "")
-    binding.pry
-    song = Song.create(name)
-    song.artist_name = artist_name
-    return song
-  end
-
-
-
-
-
-
-
-
+#   def self.new_by_name(name)
+#     song = Song.new(name)
+#   end
+# 
+#   def self.create_by_name(name)
+#     song = Song.new(name)
+#   end
+# 
+#   def self.find_by_name(name)
+#     @@all.find {|x| x.name == name}
+#   end
+# 
+#   def self.find_or_create_by_name(name)
+#     song = Song.new(name)
+#     @@all.find {|x| x.name == name}
+#   end
+# 
+#   def self.alphabetical
+#     @@all = @@all.sort do |a, b|
+#       a.name <=> b.name
+#       if a.name == b.name
+#         0
+#       elsif a.name < b.name
+#         -1
+#       elsif a.name > b.name
+#         1
+#       end
+#     end
+#     return @@all
+#   end
+# 
+#   def self.new_from_filename(mp3)
+#     name = mp3.split(" - ")
+#     artist_name = name[0]
+#     name = name[1].gsub(".mp3", "")
+#     song = Song.new(name)
+#     song.artist_name = artist_name
+#     return song
+#   end
+# 
+# 
+#   def self.destroy_all
+#     all.clear
+#   end
+# 
+#   def self.create_from_filename(mp3)
+#     name = mp3.split(" - ")
+#     artist_name = name[0]
+#     name = name[1].gsub(".mp3", "")
+#     binding.pry
+#     song = Song.create(name)
+#     song.artist_name = artist_name
+#     return song
+#   end
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
 end
