52ae1d3a0a2660d3435c5bebe14a25fc9b0b3a93
David masterson <david73180@gmail.com>
2018-10-12 20:04:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,27 +19,12 @@ class Song
   end
 
   def self.create(name)
-    song = Song.new
+    song = self.new
+    song.name = name
     return song
   end
 
-  def self.new_by_name(name)
-    song = Song.new
-    song.name = name
-  end
 
-  def self.create_by_name(name)
-    song = Song.new(name)
-  end
-
-  def self.find_by_name(name)
-    @@all.find {|x| x.name == name}
-  end
-
-  def self.find_or_create_by_name(name)
-    song = Song.new(name)
-    @@all.find {|x| x.name == name}
-  end
 
   def self.alphabetical
     @@all = @@all.sort do |a, b|
