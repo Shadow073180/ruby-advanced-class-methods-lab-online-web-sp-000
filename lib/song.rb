0c50de13eb67b797815c0d1fc76e841578722849
David masterson <david73180@gmail.com>
2018-10-12 19:41:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,20 +26,20 @@ class Song
 #   def self.new_by_name(name)
 #     song = Song.new(name)
 #   end
-# 
+#
 #   def self.create_by_name(name)
 #     song = Song.new(name)
 #   end
-# 
+#
 #   def self.find_by_name(name)
 #     @@all.find {|x| x.name == name}
 #   end
-# 
+#
 #   def self.find_or_create_by_name(name)
 #     song = Song.new(name)
 #     @@all.find {|x| x.name == name}
 #   end
-# 
+#
 #   def self.alphabetical
 #     @@all = @@all.sort do |a, b|
 #       a.name <=> b.name
@@ -53,7 +53,7 @@ class Song
 #     end
 #     return @@all
 #   end
-# 
+#
 #   def self.new_from_filename(mp3)
 #     name = mp3.split(" - ")
 #     artist_name = name[0]
@@ -62,12 +62,12 @@ class Song
 #     song.artist_name = artist_name
 #     return song
 #   end
-# 
-# 
+#
+#
 #   def self.destroy_all
 #     all.clear
 #   end
-# 
+#
 #   def self.create_from_filename(mp3)
 #     name = mp3.split(" - ")
 #     artist_name = name[0]
@@ -77,12 +77,12 @@ class Song
 #     song.artist_name = artist_name
 #     return song
 #   end
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
+#
+#
+#
 end
